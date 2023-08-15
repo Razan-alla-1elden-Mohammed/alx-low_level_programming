abce8f1851767b68b127cbaf549226d924a1bdf1
Razan-alla-1elden-Mohammed <razanalaa602@gmail.com>
2023-08-14 18:10:27 -0700

    f c 55

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
   * main -A a program that prints a line with the printf function
   * Return: 0 (success)
   */
- int main(void)
- {
-   printf("with proper grammar, but the outcome is a piece of art,\n");
-   return (0);
- }
+  int main(void)
+  {
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+  } 
 
