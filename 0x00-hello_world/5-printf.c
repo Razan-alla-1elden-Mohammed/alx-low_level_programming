8e460f6dc165f9509dc5267ba1d513544b5312fd
Razan-alla-1elden-Mohammed <razanalaa602@gmail.com>
2023-08-14 18:29:40 -0700

    file c successfully 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /**
-  * main -A a program that prints a line with the printf function
+  * main -Entry point
+  *
+  * Description: A c program that prints a line using printf function.
+  *
   * Return: 0 (success)
   */
-  int main(void)
-  {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
-  } 
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
 
