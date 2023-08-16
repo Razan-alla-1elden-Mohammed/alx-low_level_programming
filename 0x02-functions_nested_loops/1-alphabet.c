f304f30f6700fdf41b0f0e22f0909af96e95a9ee
Razan-alla-1elden-Mohammed <razanalaa602@gmail.com>
2023-08-16 14:36:53 -0700

    efje

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,13 @@
 /**
  * print_alphabet -utilizes on the _putchar funtion to print a-z
  *
- * Return 0
+ *
  */
 
 void print_alphabet(void)
 {
-int ch;
-
-for (ch = 'a'; ch <= 5; ch++)
-{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
-}
 _putchar('\n');
 }
