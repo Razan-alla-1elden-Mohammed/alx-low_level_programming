1ef6915a1a03fe4f4bdc547b48be8f4d255e705b
Razan-alla-1elden-Mohammed <razanalaa602@gmail.com>
2023-08-16 19:13:41 -0700

    ek

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - function.
+ *
+ * @n: the character
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
