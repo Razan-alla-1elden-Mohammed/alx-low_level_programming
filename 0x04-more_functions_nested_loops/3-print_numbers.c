bc784969be88b232731e902f94ede7833005a581
Razan-alla-1elden-Mohammed <razanalaa602@gmail.com>
2023-08-17 18:58:50 -0700

    eko

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - funtion.
+ *
+ * Return: 0 successful.
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
