3d33c8a2635531ea25f546259786f545e5ae6875
Razan-alla-1elden-Mohammed <razanalaa602@gmail.com>
2023-08-17 18:51:03 -0700

    ekel

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - function.
+ *
+ * @a: the parameters.
+ *
+ * @b: the b.
+ *
+ * Return: 0 success.
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
