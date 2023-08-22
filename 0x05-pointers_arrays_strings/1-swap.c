80b4497ca2eb5b783ff563518369520a7b21ed66
Razan-alla-1elden-Mohammed <razanalaa602@gmail.com>
2023-08-22 16:57:44 -0700

    eleo

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function.
+ * @a: character.
+ * @b: character.
+ * Return 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+	
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
